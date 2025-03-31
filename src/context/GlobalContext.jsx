b6ef17bb32be8cdf9b9259c9c5fec230288d1dba
Marco Mechini <mechinimarco35@gmail.com>
2025-03-31 16:55:30 +0200

    feat: creazione global context e TaskListPage

---
+++ src/context/GlobalContext.jsx
@@ -0,0 +1,35 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import useTask from "../hooks/useTasks";
+
+const GlobalContext = createContext()
+
+const api = import.meta.env.VITE_API_URL
+
+function GlobalProvider({ children }) {
+
+    const [tasks, addTask, removeTask, updateTask] = useTask()
+
+
+
+    // useEffect(() => {
+    // }, [])
+
+    const globalProviderValue = {
+        tasks, addTask, removeTask, updateTask
+    }
+
+    return (
+        <GlobalContext.Provider value={globalProviderValue}>
+            {children}
+        </GlobalContext.Provider>
+    )
+
+}
+
+
+function useGlobalContext() {
+    return useContext(GlobalContext)
+}
+
+
+export { useGlobalContext, GlobalProvider }
--- src/pages/TaskListPage.jsx
+++ src/pages/TaskListPage.jsx
@@ -1,7 +1,27 @@
+import TaskRow from "../components/TaskRow"
+import { useGlobalContext } from "../context/GlobalContext"
+
 export default function TaskListPage() {
+
+    const { tasks } = useGlobalContext()
+    console.log(tasks);
+
     return (
         <>
-            <div>Task</div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>Stato</th>
+                        <th>Data di creazione</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {tasks.map(t =>
+                        <TaskRow key={t.id} props={t}></TaskRow>
+                    )}
+                </tbody>
+            </table>
         </>
     )
 }
